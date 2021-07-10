81858e2be2b84655bd86de2386e81c89a7016b11
bluVshal <blu.vishal@gmail.com>
2021-07-10 23:13:12 +0400

    rename variable for new user Id

--- src/controllers/postControllers.js
+++ src/controllers/postControllers.js
@@ -2,15 +2,15 @@ const db = require('../db');
 const config = require('../config');
 
 async function addNewUser(newUser){
-    const getResult = await db.query('SELECT idusers FROM users ORDER BY idusers DESC LIMIT 1');
-    const resultId = JSON.parse(JSON.stringify(getResult))[0].idusers + 1;
+    const getResult = await db.query('SELECT idusers FROM users ORDER BY idusers DESC LIMIT 1'); // get the id of the newest user created
+    const newUserId = JSON.parse(JSON.stringify(getResult))[0].idusers + 1; // for automated new id while adding a new user.
     const result = await db.query(
         `INSERT INTO users 
         (idusers, uname, password, phone, address, firstname, lastname, email, photo) 
         VALUES 
         (?, ?, ?, ?, ?, ?, ?, ?, ?)`, 
         [
-            resultId, 
+            newUserId, 
             newUser.uname, 
             newUser.password, 
             newUser.phone, 
