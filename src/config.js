c80a1fcc552a602cf37a1d7b9d30faecc85e4035
bluVshal <blu.vishal@gmail.com>
2024-02-06 14:02:28 +0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const config = {
   db: { /* details entered here is for demo, configuration of db will replace the values */
     host: env.DB_HOST || 'localhost',
     user: env.DB_USER || 'root',
-    password: env.DB_PASSWORD || 'jq88esfx',
+    password: env.DB_PASSWORD || '********',
     database: env.DB_NAME || 'sys'
   },
   listPerPage: env.LIST_PER_PAGE || 10,
